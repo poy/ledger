512211248ae0ae45ea46c4c569c771982112a062
Andrew Poydence <apoydence@gmail.com>
2015-11-13 12:54:11 -0700

    Splits up regexp filter to an accounts and title filter

--- filters/regexp_filter.go
+++ filters/regexp_filter.go
@@ -7,26 +7,42 @@ import (
 )
 
 func init() {
-	AddToStore("regexp", FilterFactoryFunc(NewRegexp))
+	AddToStore("regexp-title", FilterFactoryFunc(func(pattern string) (database.Filter, error) {
+		return NewRegexp(pattern, true)
+	}))
+
+	AddToStore("regexp-accounts", FilterFactoryFunc(func(pattern string) (database.Filter, error) {
+		return NewRegexp(pattern, false)
+	}))
 }
 
 type Regexp struct {
-	reg *regexp.Regexp
+	reg        *regexp.Regexp
+	matchTitle bool
 }
 
-func NewRegexp(pattern string) (database.Filter, error) {
+func NewRegexp(pattern string, matchTitle bool) (database.Filter, error) {
 	reg, err := regexp.Compile(pattern)
 	return &Regexp{
-		reg: reg,
+		reg:        reg,
+		matchTitle: matchTitle,
 	}, err
 }
 
 func (r *Regexp) Filter(t *transaction.Transaction) []*transaction.Account {
+	if r.matchTitle {
+		return r.checkTitle(t)
+	}
+
+	return r.checkAccounts(t.Accounts)
+}
+
+func (r *Regexp) checkTitle(t *transaction.Transaction) []*transaction.Account {
 	if r.reg.MatchString(t.Title.Value) {
 		return t.Accounts.Accounts
 	}
 
-	return r.checkAccounts(t.Accounts)
+	return nil
 }
 
 func (r *Regexp) checkAccounts(accounts *transaction.AccountList) []*transaction.Account {
--- filters/regexp_filter_test.go
+++ filters/regexp_filter_test.go
@@ -13,33 +13,33 @@ var _ = Describe("RegexpFilter", func() {
 		t *transaction.Transaction
 	)
 
-	Context("Matching Title", func() {
-		BeforeEach(func() {
-			t = &transaction.Transaction{
-				Title: &transaction.Title{
-					Value: "xxyyzz",
-				},
-				Accounts: &transaction.AccountList{
-					Accounts: []*transaction.Account{
-						{
-							Name: "Expenses:Food:FastFood",
-						},
-						{
-							Name: "Liabilities:Visa",
-						},
+	BeforeEach(func() {
+		t = &transaction.Transaction{
+			Title: &transaction.Title{
+				Value: "xxyyzz",
+			},
+			Accounts: &transaction.AccountList{
+				Accounts: []*transaction.Account{
+					{
+						Name: "Expenses:Food:FastFood",
+					},
+					{
+						Name: "Liabilities:Visa",
 					},
 				},
-			}
-		})
+			},
+		}
+	})
 
+	Context("Matching Title", func() {
 		It("returns all the accounts for a title that matches", func() {
-			filter, err := filters.NewRegexp(`xxy{2}zz`)
+			filter, err := filters.NewRegexp(`xxy{2}zz`, true)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(filter.Filter(t)).To(HaveLen(2))
 		})
 
 		It("returns false for a non-matching title", func() {
-			filter, err := filters.NewRegexp(`something-else`)
+			filter, err := filters.NewRegexp(`Expenses`, true)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(filter.Filter(t)).To(HaveLen(0))
 		})
@@ -47,25 +47,29 @@ var _ = Describe("RegexpFilter", func() {
 
 	Context("Matching Accounts", func() {
 		It("returns true for the first account name that matches", func() {
-			filter, err := filters.NewRegexp(`Expenses\:\w+\:FastFood`)
+			filter, err := filters.NewRegexp(`Expenses\:\w+\:FastFood`, false)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(filter.Filter(t)).To(HaveLen(1))
 		})
 
 		It("returns true for the second account name that matches", func() {
-			filter, err := filters.NewRegexp(`Visa`)
+			filter, err := filters.NewRegexp(`Visa`, false)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(filter.Filter(t)).To(HaveLen(1))
 		})
 
 		It("returns false for non-matching account names", func() {
-			filter, err := filters.NewRegexp(`MasterCard`)
+			filter, err := filters.NewRegexp(`xxyyzz`, false)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(filter.Filter(t)).To(HaveLen(0))
 		})
 	})
 
-	It("registers itself with the store", func() {
-		Expect(filters.Fetch("regexp")).To(HaveLen(1))
+	It("registers title version with the store", func() {
+		Expect(filters.Fetch("regexp-title")).To(HaveLen(1))
+	})
+
+	It("registers accounts version with the store", func() {
+		Expect(filters.Fetch("regexp-accounts")).To(HaveLen(1))
 	})
 })
